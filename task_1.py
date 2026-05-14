e9f1cc1674c4de2f9a5a914bf436238fbfc445b1
Krisl1107 <Klagoda_zeya@mail.ru>
2026-05-14 08:29:06 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,18 +1,53 @@
-class Dog:
+class Circle:
     """
-    Dog class represents a dog with a name.
+    Represents a circle with a specified radius.
 
-    Methods:
-        say() - prints the phrase "Гав!"
+    Class Attributes:
+        pi (float): The mathematical constant Pi, used for calculations.
+        all_circles (list): A list to keep track of all instantiated Circle objects.
+
+    Instance Attributes:
+        radius (float): The radius of the circle.
     """
-    def __init__(self, name):
-        self.name = name
 
-    def say(self):
-        print("Гав!")
+    pi = 3.1415
+    all_circles = []
 
+    def __init__(self, radius=1):
+        """
+        Initializes a new Circle instance with a given radius.
 
-small_dog = Dog("Teddy")
-print(small_dog.name)
-small_dog.say()
-print("The dog's name is", small_dog.name)
+        Args:
+            radius (float, optional): The radius of the circle. Defaults to 1.
+        """
+        self.radius = radius
+        Circle.all_circles.append(self)
+
+    def area(self):
+        """
+        Calculates the area of the circle.
+
+        Returns:
+            float: The area of the circle.
+        """
+        return Circle.pi * (self.radius ** 2)
+
+    @staticmethod
+    def total_area():
+        return sum(circle.area() for circle in Circle.all_circles)
+
+    def __str__(self):
+        return str(self.radius)
+
+    def __repr__(self):
+        return str(self.radius)
+
+c1 = Circle()
+c2 = Circle(7)
+c3 = Circle(5)
+print(c2.area())
+print(c3)
+print(Circle.pi)
+print(Circle.all_circles)
+print(Circle.total_area())
+print(len(c3.__class__.all_circles))
